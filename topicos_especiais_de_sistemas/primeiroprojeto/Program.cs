82985fc7fccdbe1e21c96c1e02aa434d075ee466
Pia <algauer.henrique@gmail.com>
2024-03-05 19:00:33 -0300

    primeiro projeto

--- topicos_especiais_de_sistemas/primeiroprojeto/Program.cs
+++ topicos_especiais_de_sistemas/primeiroprojeto/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+Cliente objCliente = new Cliente();
+
+objCliente.nome = "Pedro";
+objCliente.cpf = "888";
+
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine(objCliente.nome);
