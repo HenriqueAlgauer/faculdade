313b2564f91a693534a2061fe0223958f7e0c2be
Pia <algauer.henrique@gmail.com>
2024-03-06 19:57:41 -0300

    exercicio 2

---
+++ desenvolvimento_de_software/exercicios/estruturas_repeticao/exe1.java
@@ -0,0 +1,12 @@
+package exercicios.estruturas_repeticao;
+
+class exe1 {
+
+    public static void main(String[] args){
+        byte num = 0; 
+        while(num<11){
+            System.out.print(num+"\n");
+            num++;
+        }
+    }
+}
---
+++ desenvolvimento_de_software/exercicios/estruturas_repeticao/exe2.java
@@ -0,0 +1,11 @@
+class exe2 {
+    public static void main(String[] args){
+        int soma = 0;
+
+        for(int i=1; i<=100;i++){
+            System.out.print("\n"+soma+ "\n");
+            soma += i;
+        }
+        System.out.print(soma);
+    }
+}
