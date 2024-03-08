3ae29e0b6944d256852029253ed7ce025df7e463
Pia <algauer.henrique@gmail.com>
2024-03-08 19:37:28 -0300

    exe6

--- desenvolvimento_de_software/exercicios/estruturas_repeticao/exe6.java
+++ desenvolvimento_de_software/exercicios/estruturas_repeticao/exe6.java
@@ -1,9 +1,15 @@
 class exe6 {
     public static void main(String[]args){
-        
+        int atual = 1;
+        int anterior = 1;
+        System.out.printf( anterior+ " " + atual);
         for(int i =0; i<11;i++){
-            System.out.print("oi");
+            //int novo = atual + anterior;
+            int aux = atual;
+            atual += anterior;
+            System.out.printf(" %d ", atual);
             
+            anterior = aux;
         }
     }
 }
