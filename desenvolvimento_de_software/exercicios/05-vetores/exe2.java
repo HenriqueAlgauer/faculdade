29d36d0e01d4ac78217c0c4f116a84d2559cbfee
henriqueAlgauer <algauer.henrique@gmail.com>
2024-03-17 17:12:58 -0300

    ajuste exe 02

--- desenvolvimento_de_software/exercicios/05-vetores/exe2.java
+++ desenvolvimento_de_software/exercicios/05-vetores/exe2.java
@@ -1,6 +1,6 @@
 public class exe2 {
     public static void main(String[] args){
-        float[] numbers = {1,1,1,10,100};
+        float[] numbers = {20,1,17,10,20};
         float soma = 0;
 
         for(int i = 0; i < numbers.length; i++){
@@ -9,5 +9,13 @@ public class exe2 {
         
         double media = soma/numbers.length;
         System.out.printf("Media: %.2f", media);
+        System.out.println("\nNumeros maiores ou igual a media: ");
+        
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] >= media){
+                System.out.print(numbers[i]+" | ");
+            }
+        }
+
     }
 }
