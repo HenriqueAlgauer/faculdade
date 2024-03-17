3e789b8b530e9101a815539461fd128eddd46b2e
henriqueAlgauer <algauer.henrique@gmail.com>
2024-03-17 03:15:17 -0300

    exe 05 finalizado

--- desenvolvimento_de_software/exercicios/04-orientação-objetos/exe05.java
+++ desenvolvimento_de_software/exercicios/04-orientação-objetos/exe05.java
@@ -1,6 +1,26 @@
 //Escreva um método que retorne o maior valor entre três números inteiros.
+class Numeros{
+    int maior;
 
+    int maiorValor(int num1, int num2, int num3){
+        this.maior = num1;
+
+        if(this.maior < num2){
+            this.maior = num2;
+        }
+        if(this.maior < num3){
+            this.maior = num3;
+        }
+
+        return this.maior;
+    }
+}
 
 public class exe05 {
+    public static void main(String[] args){
+        Numeros n1 = new Numeros();
 
+        n1.maiorValor(90, 20, 30);
+        System.out.print("Maior número: " + n1.maior);
+    }
 }
