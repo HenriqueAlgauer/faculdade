f14a5cabb24ddafb91fd899f4f8149ce977a6b4e
Pia <algauer.henrique@gmail.com>
2024-03-22 19:25:24 -0300

    exe 02

---
+++ desenvolvimento_de_software/exercicios/08-poo-atividades-extras/pooexe02.java
@@ -0,0 +1,7 @@
+//Em um mundo cada vez mais digital, um grupo de amigos apaixonados por videogames percebe uma oportunidade de combinar seus hobbies com a prática de programação. Eles decidem criar um sistema para gerenciar perfis de jogadores em seus jogos favoritos, algo que permitiria acompanhar o progresso, as conquistas e as estatísticas de cada jogador de forma personalizada e automatizada. 
+
+// Inspirado por esse projeto entre amigos, o desafio é desenvolver uma parte desse sistema de gerenciamento de jogadores utilizando Java, com um foco especial na orientação a objetos. O sistema deve permitir a criação de perfis de jogadores, armazenando informações como nome, pontuação e nível. Além disso, deve ser possível atualizar a pontuação e o nível dos jogadores no sistema. 
+
+class pooexe02 {
+    
+}
