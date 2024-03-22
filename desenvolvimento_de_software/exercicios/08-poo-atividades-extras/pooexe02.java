ed0d4f80931316aad397b0ad949cc3d0e45a6e62
Pia <algauer.henrique@gmail.com>
2024-03-22 19:41:34 -0300

    exe 02 finalizado

--- desenvolvimento_de_software/exercicios/08-poo-atividades-extras/pooexe02.java
+++ desenvolvimento_de_software/exercicios/08-poo-atividades-extras/pooexe02.java
@@ -2,6 +2,37 @@
 
 // Inspirado por esse projeto entre amigos, o desafio é desenvolver uma parte desse sistema de gerenciamento de jogadores utilizando Java, com um foco especial na orientação a objetos. O sistema deve permitir a criação de perfis de jogadores, armazenando informações como nome, pontuação e nível. Além disso, deve ser possível atualizar a pontuação e o nível dos jogadores no sistema. 
 
+class Player{
+    String name;
+    int points;
+    int level;
+
+    void changePoints(int points){
+        this.points += points;
+    }
+    void changeLevel(int level){
+        this.level += level;
+    }
+    void status(){
+        System.out.println("Name: " + this.name);
+        System.out.println("Points: " + this.points);
+        System.out.println("Level: " + this.level);
+    }
+}
+
 class pooexe02 {
-    
+    public static void main(String[] args){
+        Player p1 = new Player();
+
+        p1.name = "John Doe";
+        p1.level = 14;
+        p1.points = 2000;
+
+        p1.status();
+
+        p1.changeLevel(2);
+        p1.changePoints(400);
+
+        p1.status();
+    }
 }
