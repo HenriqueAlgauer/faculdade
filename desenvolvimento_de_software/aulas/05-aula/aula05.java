3f78d0f9b66cee5cb352d3c57e9aa3f382d60e7d
Pia <algauer.henrique@gmail.com>
2024-04-10 19:24:46 -0300

    adicionado classe departamento

--- desenvolvimento_de_software/aulas/05-aula/aula05.java
+++ desenvolvimento_de_software/aulas/05-aula/aula05.java
@@ -1,3 +1,38 @@
 class aula05 {
+    public static void main(String[] args){
 
+    }
+}
+
+class Funcionario{
+    private String nome;
+    private String cargo;
+
+    public Funcionario(String nome, String cargo){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+}
+
+class Departamento{
+    private String nome;
+    private Funcionario[] funcionarios;
+    private int numFuncionarios;
+
+    public Departamento(String nome){
+        this.nome = nome;
+        this.funcionarios = new Funcionario[10];
+        this.numFuncionarios = 0;
+    }
+
+    public void addFuncionario(Funcionario funcionario){
+        funcionarios[numFuncionarios] = funcionario;
+        numFuncionarios++;
+    }
 }
