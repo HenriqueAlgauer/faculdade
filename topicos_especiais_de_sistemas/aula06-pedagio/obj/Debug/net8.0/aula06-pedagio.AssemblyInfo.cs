b482efe8fd5d81bae7f069af424e03ceccf88500
Pia <algauer.henrique@gmail.com>
2024-04-17 17:28:50 -0300

    alteração

--- topicos_especiais_de_sistemas/aula06-pedagio/obj/Debug/net8.0/aula06-pedagio.AssemblyInfo.cs
+++ topicos_especiais_de_sistemas/aula06-pedagio/obj/Debug/net8.0/aula06-pedagio.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("primeiroprojeto")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("aula06-pedagio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b71f34f25b9ec83eb19a38fbf77cc224d46e45")]
-[assembly: System.Reflection.AssemblyProductAttribute("primeiroprojeto")]
-[assembly: System.Reflection.AssemblyTitleAttribute("primeiroprojeto")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7fa01aff9502992a5a17e12f59a1e78dc701dc0")]
+[assembly: System.Reflection.AssemblyProductAttribute("aula06-pedagio")]
+[assembly: System.Reflection.AssemblyTitleAttribute("aula06-pedagio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
