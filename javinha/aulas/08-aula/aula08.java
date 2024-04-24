00afe76608b287a19479f8dbbeef1ea20d29b0fc
Pia <algauer.henrique@gmail.com>
2024-04-24 19:36:16 -0300

    alterado nome pasta

---
+++ javinha/aulas/08-aula/aula08.java
@@ -0,0 +1,24 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class aula08 {
+    public static void main(String[] args){
+            
+        Map <String, Integer> estoque = new TreeMap<>();
+        estoque.put("Maçã", 10);
+        estoque.put("Laranja", 20);
+        estoque.put("Banana", 30);
+
+        mostrarEstoque(estoque);
+
+        if(estoque.containsKey("Maçã")){
+            System.out.println("Tem maçã no estoque");
+        }
+    }
+
+    public static void mostrarEstoque(Map<String, Integer> estoque){
+        for(Map.Entry<String, Integer> fruta : estoque.entrySet()){
+            System.out.println("Fruta: " + fruta.getKey() + " || Quantidade: " + fruta.getValue());
+        }
+    }
+}
