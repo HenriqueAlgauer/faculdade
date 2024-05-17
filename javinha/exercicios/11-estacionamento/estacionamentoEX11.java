77b8b8279d7defc09a4459d1bae0a5c5c5559294
Pia <algauer.henrique@gmail.com>
2024-05-17 19:26:55 -0300

    exercicio 11

--- javinha/exercicios/11-estacionamento/estacionamentoEX11.java
+++ javinha/exercicios/11-estacionamento/estacionamentoEX11.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class estacionamento{
+
+}
+
 public class estacionamentoEX11 {
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<String> lista = new ArrayList<String>();
+        do{
+
+            System.out.println("Oi");
+        }
+        while(false);
+
+
+        System.out.println("Hello");
     }
 }
