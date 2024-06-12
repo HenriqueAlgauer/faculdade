2ca31f36afe30f641771329181b93de98cfd3b68
Pia <algauer.henrique@gmail.com>
2024-06-12 19:56:16 -0300

    .

---
+++ javinha/provaFinal/src/Produto.java
@@ -0,0 +1,39 @@
+public class Produto {
+    private int codigoProduto;
+    private String nomeProduto;
+    private double precoUnitario;
+    private int quantidadeEstoque;
+
+    public Produto(int cod, String nome, double preco, int estoque){
+        setCodigoProduto(cod);
+        setNomeProduto(nome);
+        setPrecoUnitario(preco);
+        setQuantidadeEstoque(estoque);
+    }
+
+    public void setCodigoProduto(int codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public int getCodigoProduto() {
+        return codigoProduto;
+    }
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+}
